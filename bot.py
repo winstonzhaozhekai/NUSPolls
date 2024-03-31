3189c833e4551c96bac5ea4195a2b2c98d9f23e1
winstonzhaozhekai <winstonzhaozhekai@gmail.com>
2024-03-31 13:14:26 +0800

    Added Info

--- bot.py
+++ bot.py
@@ -10,9 +10,15 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello")
 
+async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Welcome to NUSPolls, a completely anonymous platform for NUS Students to ask their heartburning questions. \n"
+    )
+
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler('start', start))
+    app.add_handler(CommandHandler('info', info))
 
     app.run_polling()
 
