492a41bc976c2b2e385b8f05350fd3ae9fb309b1
winstonzhaozhekai <winstonzhaozhekai@gmail.com>
2024-03-31 11:49:18 +0800

    First Update

--- bot.py
+++ bot.py
@@ -1,34 +1,20 @@
 from dotenv import load_dotenv
 import os
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-# Define the start command handler
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm a Telegram bot.")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello")
 
-# Define the message handler
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-# Set up the bot handlers
 def main():
-    updater = Updater(token=BOT_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler('start', start))
 
-    start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(filters.text & ~filters.command, echo)
-
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(echo_handler)
-
-    updater.start_polling()
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
-
-
-
